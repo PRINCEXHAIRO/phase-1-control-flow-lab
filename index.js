25681ffd333d14b07b180a32ebddfcac3d12cb85
Hairo Barnard <Hairo.barnard@gmail.com>
2023-11-01 23:13:48 -0400

    Completed Lab

--- index.js
+++ index.js
@@ -1,11 +1,34 @@
-function scuberGreetingForFeet(){
-  // Write your code here!
-}
+function scuberGreetingForFeet(ride) {
 
-function ternaryCheckCity(){
-  // Write your code here!
-}
+  if (ride<=400) { 
+    return 'This one is on me!'
+  } else if (ride<=2000) {
+      return "That will be twenty bucks."
+  } else if(ride<=2500) {
+     return 'I will gladly take your thirty bucks.'
+  } else{
+     return 'No can do.'
+  };
+};
+console.log(scuberGreetingForFeet(44))
 
-function switchOnCharmFromTip(){
+function ternaryCheckCity(city) { 
+
+  const checkIt = city === "NYC" ? 'Ok, sounds good.' : 'No go.' ;
+  return checkIt;
+};
+console.log(ternaryCheckCity("NYC"))
+
+function switchOnCharmFromTip(whatsTheTip){
   // Write your code here!
-}
+  switch (whatsTheTip) { 
+    case 'generous': 
+        return 'Thank you so much.'
+    case 'not as generous':
+        return 'Thank you.'
+    default: 
+        return 'Bye.'
+  };
+};
+
+console.log('generous')
